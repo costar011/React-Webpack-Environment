a032dd1b22b4f72ab1c1c7629890d6d65d1514fd
YERIM <dnaer456@gmail.com>
2020-07-22 15:31:13 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+import axios from "axios";
+import Memo from "./components/Memo";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return <div>Test Page</div>;
   }
